e414a6ce9d5a1d81b20c70e1d3d9a4716d0b62dc
Kattrxvjxxde <sabo.0038.com@gmail.com>
2018-07-19 14:36:14 +0900

    Modify root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # devise
   devise_for :users
-  devise_scope :user do
-    root to: 'devise/sessions#new'
+
+  # root
+  unauthenticated :user do # 未ログイン時はログイン画面をrootとする
+    devise_scope :user do
+      root to: 'devise/sessions#new'
+    end
   end
+  root to: 'users#show'
 
   # user page
   resources :users
